457c71458f2f5b4c64d97b83f98d68ad8338aca4
“Ryan <ryan.yee333@gmail.com>
2024-11-05 06:01:38 -0800

    Added Initial Conditions

--- Project/code/SIZE.h
+++ Project/code/SIZE.h
@@ -44,7 +44,7 @@ C     Nr  :: No. points in Z for full process domain.
       INTEGER Ny
       INTEGER Nr
       PARAMETER (
-     &           sNx =  240,
+     &           sNx =  260,
      &           sNy =  360,
      &           OLx =   4,
      &           OLy =   4,
